9bf8a432746d117f3db451493dce4480ede6317a
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2024-04-01 21:38:47 +0300

    major changes

--- softjam entry/Assets/scripts/PlayerMortality.cs
+++ softjam entry/Assets/scripts/PlayerMortality.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMortality : MonoBehaviour
 {
@@ -8,19 +9,22 @@ public class PlayerMortality : MonoBehaviour
 
     private PlayerMovement player;
 
+    public UnityEvent OnDeath;
+
     private void Start()
     {
         player = GetComponent<PlayerMovement>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public IEnumerator OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 6)
         {
+            OnDeath.Invoke();
+            yield return new WaitForEndOfFrame();
             ReturnToCheckpoint();
         }
     }
-
 
     public void ReturnToCheckpoint()
     {
